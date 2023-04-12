21007a7f0a7b89f6b8d07b8676e68187b9d00c3e
kanghayeon <khy3039@naver.com>
2023-04-13 04:48:28 +0900

    - 공지의 요약(80자) 보여주기 - inmun과 cns 단과대학 모든 공지 카테고리별로 크롤링 - 공지의 날짜부분 yyyy/mm/dd 형식으로 포맷팅

--- app/src/main/java/com/example/gnumoa_hayeon/NoticeAdapter.kt
+++ app/src/main/java/com/example/gnumoa_hayeon/NoticeAdapter.kt
@@ -5,7 +5,12 @@ import android.view.ViewGroup
 import android.widget.ImageButton
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.Timestamp
 import com.google.firebase.firestore.FirebaseFirestore
+import java.text.SimpleDateFormat
+import java.util.*
+
+
 
 // : -> 상속
 // 1. Notice_list 데이터 클래스를 들고와서 ArrayList로 리스트화 시킨 것을 noticeList 변수에 넣음
@@ -14,20 +19,61 @@ class NoticeAdapter : RecyclerView.Adapter<NoticeAdapter.NoticeViewHolder>() {
 
     val noticeList: ArrayList<Notice_list> = arrayListOf()
 
+    //요약 보여주기 - 공지 전체내용 중 일부(80자)만 가져옴
+    //context가 null 또는 empty가 아니라면 fullText 값을 계산한 후 80자 이상인 경우 일부를 자르고 "..."을 붙임
+    private fun getContextPreview(context: ArrayList<String>?): String {
+        if (!context.isNullOrEmpty()) {
+            val fullText = context[0]
+            return if (fullText.length > 80) "${fullText.substring(0, 80)}..." else fullText
+        }
+        return ""
+    }
+
+
+    // documents와 collections1, collections2 리스트를 만들어서 중첩 반복문으로 각각의 문서와 컬렉션을 가져와서 SnapshotListener를 등록
+    // 리스트에 있는 모든 컬렉션들의 문서 데이터가 noticeList에 추가
+    // notifyDataSetChanged() 함수를 호출하여 RecyclerView를 업데이트
     init {
+        val inmunDocuments = listOf("dokmun", "korea", "china", "english", "france", "hanmun", "his", "korea", "minsok", "russia", "sophia")
+        val inmunCollections = listOf("공지사항", "기타", "장학", "수업", "졸업", "학사", "행사")
+
+        val cnsDocuments = listOf("biomat", "chem", "cloth", "cs", "cse", "foodnutri", "geology", "ls", "math", "pharmgine", "psysics", "stat")
+        val cnsCollections = listOf("공지사항", "기타", "장학", "수업", "졸업", "학사", "행사")
+
         val firestore = FirebaseFirestore.getInstance()
 
-        firestore?.collection("inmun")?.document("dokmun")?.collection("기타")
-            ?.addSnapshotListener { querySnapshot, firebaseFirestoreException ->
-                noticeList.clear()
+// inmun 컬렉션의 문서 데이터 가져오기
+        for (document in inmunDocuments) {
+            for (collection in inmunCollections) {
+                firestore.collection("inmun").document(document).collection(collection)
+                    .addSnapshotListener { querySnapshot, firebaseFirestoreException ->
 
-                for (snapshot in querySnapshot!!.documents) {
-                    var item = snapshot.toObject(Notice_list::class.java)
-                    noticeList.add(item!!)
-                }
-                notifyDataSetChanged()
+                        for (snapshot in querySnapshot!!.documents) {
+                            val item = snapshot.toObject(Notice_list::class.java)
+                            noticeList.add(item!!)
+                        }
+                        notifyDataSetChanged()
+                    }
             }
+        }
+
+// cns 컬렉션의 문서 데이터 가져오기
+        for (document in cnsDocuments) {
+            for (collection in cnsCollections) {
+                firestore.collection("cns").document(document).collection(collection)
+                    .addSnapshotListener { querySnapshot, firebaseFirestoreException ->
+
+                        for (snapshot in querySnapshot!!.documents) {
+                            val item = snapshot.toObject(Notice_list::class.java)
+                            noticeList.add(item!!)
+                        }
+                        notifyDataSetChanged()
+                    }
+            }
+        }
+
     }
+
 
     //onCreateViewHolder, onBindViewHolder, getItemCount() => view 홀더가 생성되는 곳
     //NoticeViewHolder => view 홀더 정의하는 곳
@@ -50,9 +96,19 @@ class NoticeAdapter : RecyclerView.Adapter<NoticeAdapter.NoticeViewHolder>() {
         holder.major.text = noticeList.get(position).major
         holder.category.text = noticeList.get(position).category
         holder.title.text = noticeList.get(position).title
-        holder.context.text = noticeList.get(position).context.toString()
-        holder.createdAt.text = noticeList.get(position).createdAt.toString()//문자열로 변환
+        holder.context.text = getContextPreview(noticeList.get(position).context)
+        //holder.createdAt.text = noticeList.get(position).createdAt.toString()//문자열로 변환
         noticeList.get(position).heart?.let { holder.heart.setImageResource(it) }
+
+
+        // Firestore Timestamp 객체를 Date 객체로 변환
+        val timestamp = noticeList.get(position).createdAt as Timestamp
+        val date = timestamp.toDate()
+
+        // SimpleDateFormat을 사용하여 원하는 형식으로 포맷팅
+        val sdf = SimpleDateFormat("yyyy / MM / dd", Locale.getDefault())
+        holder.createdAt.text = sdf.format(date)
+
     }
 
     //전체 아이템 개수 리턴
