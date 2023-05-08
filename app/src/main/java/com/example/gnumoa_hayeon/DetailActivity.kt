0c1ab04f07d659138895282c6562af6a3031f658
hykim02 <114382247+hykim02@users.noreply.github.com>
2023-05-08 22:14:54 +0900

    공지화면 파일 하이퍼링크 처리완료

--- app/src/main/java/com/example/gnumoa_hayeon/DetailActivity.kt
+++ app/src/main/java/com/example/gnumoa_hayeon/DetailActivity.kt
@@ -1,18 +1,21 @@
 package com.example.gnumoa_hayeon
 
-import android.content.ContentValues.TAG
 import android.content.Intent
+import android.graphics.Color
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
-import android.webkit.WebChromeClient
-import android.widget.Button
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.TextPaint
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.gnumoa_hayeon.databinding.NoticeDetailBinding
 import com.google.firebase.Timestamp
-import com.google.firebase.firestore.FirebaseFirestore
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.collections.HashMap
 
 class DetailActivity :AppCompatActivity() {
     private val binding by lazy {
@@ -32,30 +35,68 @@ class DetailActivity :AppCompatActivity() {
         // SimpleDateFormat을 사용하여 원하는 형식으로 포맷팅
         val sdf = SimpleDateFormat("yy / MM / dd", Locale.getDefault())
 
-        if (data != null) {
-            binding.noticeTitle.text = data!!.title
-            binding.noticeCategory.text = data!!.category
-            binding.noticeMajor.text = data!!.major
-            binding.noticeDate.text = sdf.format(date)
-//            binding.noticeContext.text = data!!.context.toString()
-            binding.fileUrls.text = data!!.fileUrls.toString()
+        binding.noticeTitle.text = data.title
+        binding.noticeCategory.text = data.category
+        binding.noticeMajor.text = data.major
+        binding.noticeDate.text = sdf.format(date)
 
-            val url = data!!.baseUrl
+        //hashmap 형태 파일 나타내기+텍스트뷰 하이퍼링크 처리
 
-            binding.btnUrl.setOnClickListener {
-                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
-                startActivity(intent)
+        val files = data.fileUrls as HashMap<String, String>
+        val stringBuilder = SpannableStringBuilder()
+        var index = 1
+
+        for ((key, value) in files) {
+            val prefix = "$index"
+            stringBuilder.append("$prefix. $key\n")
+            // 파일 이름과 prefix를 StringBuilder에 추가
+            var isClicked = false
+            val clickableSpan = object : ClickableSpan() {
+                override fun onClick(widget: View) {
+                    isClicked = true
+                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(value))
+                    startActivity(intent)
+                }
+                override fun updateDrawState(ds: TextPaint) {
+                    super.updateDrawState(ds)
+                    if (isClicked) {
+                        ds.color = Color.rgb(139,0,255) // 클릭했을 때 색상 변경
+                    } else {
+                        ds.color = Color.BLUE // 클릭 전에는 파란색으로 유지
+                    }
+                    ds.isUnderlineText = true // 밑줄 추가
+                }
             }
-
-            val html = data!!.html
-
-            if (html != null) {
-                binding.noticeHtml.loadData(html, "text/html", "UTF-8")
-                binding.noticeHtml.loadDataWithBaseURL(null, html, "text/html", "utf-8", null)
-            }
-
-
+            //클릭 가능한 시작 부분과 끝부분 정의
+            val start = stringBuilder.indexOf("$key\n")
+            val end = start + key.length
+            stringBuilder.setSpan(clickableSpan, start, end, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+            // 파일 이름에 클릭 이벤트를 추가
+            index++
         }
+
+        binding.fileUrls.apply {
+            text = stringBuilder
+            movementMethod = LinkMovementMethod.getInstance()
+        }
+
+
+        //해당 url로 이동
+        val url = data.baseUrl
+
+        binding.btnUrl.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            startActivity(intent)
+        }
+
+        //html 웹뷰로 나타내기
+        val html = data.html
+
+        if (html != null) {
+            binding.noticeHtml.loadData(html, "text/html", "UTF-8")
+            binding.noticeHtml.loadDataWithBaseURL(null, html, "text/html", "utf-8", null)
+        }
+
 
         binding.btnBack.setOnClickListener{
             val intent = Intent(this, HomeActivity::class.java)
