55efee373dc48c34c2d20a21304572a20830ef75
hayeonkang <khy3039@naver.com>
2023-05-31 22:29:46 +0900

    내 파트 최종완료

--- app/src/main/java/com/example/gnumoa_hayeon/MajorActivity.kt
+++ app/src/main/java/com/example/gnumoa_hayeon/MajorActivity.kt
@@ -5,7 +5,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.gnumoa_hayeon.databinding.ActivityMajorBinding
 
